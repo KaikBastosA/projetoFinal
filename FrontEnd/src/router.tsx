794ac669ea064da13c6ce0ce4440de62eaffad8d
Kaik <kaikbastos@id.uff.br>
2025-03-09 18:25:10 -0300

    Adicionado o roteamento para ter o header e o footer em todas as páginas

--- FrontEnd/src/router.tsx
+++ FrontEnd/src/router.tsx
@@ -1,11 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layout";
+import Infantil from "./pages/infantil";
+import Home from "./pages/home";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Layout/>
-    }
+        element: <Layout/>,
+        children: [
+            {
+                index: true,
+                element: <Home/>
+            },
+            {
+                path: 'infantil',
+                element: <Infantil/>
+            }
+        ]
+    },
+
 ]);
 
 export default router;
