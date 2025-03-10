00d67c6a5b9175c63f3bff81db3814918eef7df4
CaiodoValeSoares <caiodovalesoares@gmail.com>
2025-03-10 16:05:16 -0300

    authenticate user

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -2,10 +2,29 @@ import fastify from "fastify";
 import { feedbacksRoutes } from "./http/controllers/feedbacks/routes";
 import { userRoutes } from "./http/controllers/users/routes";
 import { ZodError } from "zod";
-import { addressRoutes } from "./http/controllers/address/routes";
+import fastifyCors from "@fastify/cors";
+import fastifyJwt from "@fastify/jwt";
+import { env } from "./env";
 
 export const app = fastify()
 
+app.register(fastifyCors, {
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+})
+
+app.register(fastifyJwt, {
+    secret: env.JWT_SECRET
+})
+
 app.register(feedbacksRoutes)
 app.register(userRoutes)
-app.register(addressRoutes)
+
+app.setErrorHandler((error, request, reply) => {
+    if (error instanceof ZodError) {
+        return reply.status(400).send({ message: 'Erro de validação', issues: error.format()})
+    }
+    return reply.status(500).send({ message: 'Erro interno no servidor'})
+})
