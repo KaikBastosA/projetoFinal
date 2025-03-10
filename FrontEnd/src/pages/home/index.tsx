9ffaeb1cd9315de6c28652677a12228e00151e03
Kaik <kaikbastos@id.uff.br>
2025-03-10 18:56:44 -0300

    (feat): Adicionado o banner principal da home page

--- FrontEnd/src/pages/home/index.tsx
+++ FrontEnd/src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import styles from './styles.module.css';
-import logo from '../../assets/logocTitulo.svg'
+import logo from '../../assets/logocTitulo.svg';
 
-export default function Home(){
+export default function Home() {
     return (
-        <section className={styles.main}>
-            <img src={logo} alt="Pijam{IN}ha-logo" />
-            <h1>Se os lobos soubessem desse conforto, nem sopravam casas, iam dormir!</h1>
-        </section>
+        <>
+            <section className={styles.main}>
+                <img src={logo} alt="Pijam{IN}ha-logo" />
+                <h1>Se os lobos soubessem desse conforto, nem sopravam casas, iam dormir!</h1>
+            </section>
+        </>
     )
 }
