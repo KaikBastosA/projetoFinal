ba6c01ec0748655f754df2abf0086a608439fb3e
ivanc <ivandelattre7@gmail.com>
2025-03-10 15:30:21 -0300

    fix : corrije app

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -2,10 +2,9 @@ import fastify from "fastify";
 import { feedbacksRoutes } from "./http/controllers/feedbacks/routes";
 import { userRoutes } from "./http/controllers/users/routes";
 import { ZodError } from "zod";
-import { addressRoutes } from "./http/controllers/address/routes";
+
 
 export const app = fastify()
 
 app.register(feedbacksRoutes)
 app.register(userRoutes)
-app.register(addressRoutes)
