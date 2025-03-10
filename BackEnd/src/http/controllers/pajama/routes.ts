2e3150f69de15f3ec9c0dc8437d6891d34e2d6f2
CaiodoValeSoares <caiodovalesoares@gmail.com>
2025-03-10 20:52:26 -0300

    create pajama

--- BackEnd/src/http/controllers/pajama/routes.ts
+++ BackEnd/src/http/controllers/pajama/routes.ts
@@ -1,6 +1,6 @@
 
 import { FastifyInstance } from "fastify";
-import { create } from "./create";
+import { create, createMultiple } from "./create";
 import { deletePajama } from "./delete";
 import { getAll } from "./getAll";
 import { get } from "./getById";
@@ -8,7 +8,8 @@ import { get } from "./getById";
 
 export function pajamaRoutes(app: FastifyInstance) {
     app.post('/pajamas', create)
-    app.delete('/pajamas/:id', deletePajama); 
+    app.post('/pajamas/multiple', createMultiple)
+    app.delete('/pajamas/:id', deletePajama)
     app.get('/all-pajamas', getAll)
     app.get("/pajamas/:id" , get)
 }
