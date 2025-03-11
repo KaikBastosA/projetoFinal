cd4a28b48fdcff3a343be30e1b586859d0a712ed
Kaik <kaikbastos@id.uff.br>
2025-03-10 22:32:26 -0300

    (refactor): Mudado o nome da classe de main para mainBanner

--- FrontEnd/src/pages/home/index.tsx
+++ FrontEnd/src/pages/home/index.tsx
@@ -5,7 +5,7 @@ import Slide from '../../components/slide';
 export default function Home() {
     return (
         <>
-        <section className={styles.main}>
+        <section className={styles.mainBanner}>
             <div className={styles.banner}>
                 <img src={logo} alt="" />
                 <h1>Se os lobos soubessem desse conforto, nem sopravam casas, iam dormir!</h1>
