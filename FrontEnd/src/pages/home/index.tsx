e38cb4ebeb4dce4f023c53f934589c4b5ae278a4
Kaik <kaikbastos@id.uff.br>
2025-03-10 22:29:12 -0300

    (feat): Adicionado o slide na página principal e corrigido o banner quebrando de forma errada ao mudar o tamanho da tela

--- FrontEnd/src/pages/home/index.tsx
+++ FrontEnd/src/pages/home/index.tsx
@@ -1,13 +1,17 @@
 import styles from './styles.module.css';
 import logo from '../../assets/logocTitulo.svg';
+import Slide from '../../components/slide';
 
 export default function Home() {
     return (
         <>
-            <section className={styles.main}>
-                <img src={logo} alt="Pijam{IN}ha-logo" />
+        <section className={styles.main}>
+            <div className={styles.banner}>
+                <img src={logo} alt="" />
                 <h1>Se os lobos soubessem desse conforto, nem sopravam casas, iam dormir!</h1>
-            </section>
+            </div>
+        </section>
+            <Slide />
         </>
     )
 }
