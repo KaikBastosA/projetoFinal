eb6f5c88e50e128375f04ed76c0c76b32029ac12
LuizFelipeVVAlves <lfvilaverde@gmail.com>
2025-03-11 13:57:03 -0300

    feat: inicio desenv loin

--- FrontEnd/src/pages/login/login.tsx
+++ FrontEnd/src/pages/login/login.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Form from '../../components/form/form'
 import s from './login.module.css'
 
@@ -8,6 +9,13 @@ export default function login() {
 
         <Form>
             <h1>Login</h1>
+            <p>Faça login para ter acesso aos pijamas dos seus sonhos!</p>
+            <form action="">
+                <input type="text" placeholder='Usuário ou e-mail'/>
+                <input type="text" placeholder='Senha' />
+                <button>Entrar</button>
+            </form>
+            <Link to='reg/cadastro'>Cadastre-se</Link>
         </Form>
     )
 }
