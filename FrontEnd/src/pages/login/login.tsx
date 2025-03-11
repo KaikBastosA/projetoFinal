a67a8de285556e3bbef630f0b5318f4f3704203d
LuizFelipeVVAlves <lfvilaverde@gmail.com>
2025-03-11 17:12:36 -0300

    feat: página do login concluida( ainda faltam as validações) e ajustes estéticos

--- FrontEnd/src/pages/login/login.tsx
+++ FrontEnd/src/pages/login/login.tsx
@@ -8,14 +8,16 @@ export default function login() {
     return (
 
         <Form>
-            <h1>Login</h1>
-            <p>Faça login para ter acesso aos pijamas dos seus sonhos!</p>
-            <form action="">
-                <input type="text" placeholder='Usuário ou e-mail'/>
-                <input type="text" placeholder='Senha' />
-                <button>Entrar</button>
+            <h1 className={s.title}>Login</h1>
+            <p className={s.text}>Faça login para ter acesso aos pijamas dos seus <strong>sonhos!</strong></p>
+            <form action="" className={s.login_form}>
+                <input className={s.input_log} type="text" placeholder='Usuário ou e-mail'/>
+                <input className={s.input_log} type="text" placeholder='Senha' />
+                <Link className={s.senha_btn} to=''>Esqueci minha senha</Link>
+                <button className={s.entrar_btn}>Entrar</button>
             </form>
-            <Link to='reg/cadastro'>Cadastre-se</Link>
+            <hr className={s.separator} />
+            <button className={s.cadastrar_btn}><Link className={s.cadastrar_link} to='/reg/cadastro'>Cadastre-se</Link></button>
         </Form>
     )
 }
