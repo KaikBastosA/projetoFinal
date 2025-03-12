29a051d47b096eb6d1188287fa1852a909b884b7
Gabriel Estrella <gestrella@id.uff.br>
2025-03-12 12:26:49 -0300

    feat: adição da leitura de vendas

--- BackEnd/src/http/controllers/sale/routes.ts
+++ BackEnd/src/http/controllers/sale/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { create } from "./create";
+import { get } from "./get";
 
 
 
 export function saleRoutes(app: FastifyInstance) {
     app.post("/create-sale", create )
+    app.get("/get-sale/:id", get)
 }
