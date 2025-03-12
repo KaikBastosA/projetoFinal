ebedf3e0ec717c2abe71b193cadb5679f6a172f2
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-03-12 20:00:06 -0300

    (fix & style): página de favoritos componentizada e adicionado menu de navegação entre página carrinho e favoritos

--- FrontEnd/src/pages/favoritos/index.tsx
+++ FrontEnd/src/pages/favoritos/index.tsx
@@ -1,13 +1,34 @@
 import useFavoriteStore from '../../stores/FavoriteStore'
+import carrinhoClicado from '../../assets/carrinho-de-comprar-icon.svg'
+import carrinhoNaoClicado from '../../assets/carrinho-de-compras-cinza-icon.svg'
+import favoritoClicado from '../../assets/favorite-heart-icon.svg'
 import styles from './styles.module.css'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Favoritos() {
 
     const { favorites } = useFavoriteStore()
+    const navigate = useNavigate() 
+    const [ carrinhoAtivo, setCarrinhoAtivo] = useState(false)
+
+    function handleClick() {
+        setCarrinhoAtivo(true)
+        navigate('/carrinho')
+    }
 
     return(
         <div>
-            <h1>Meus favoritos</h1>
+            <div className={styles.nav}>
+                <button onClick={handleClick} className={styles.carrinhoBotao}>
+                    <img src={carrinhoAtivo ? carrinhoClicado : carrinhoNaoClicado} alt="" />
+                    Carrinho
+                </button>
+                <button className={styles.favoritoBotao}>
+                    <img src={favoritoClicado} alt="" />
+                    Favoritos
+                </button>
+            </div>
             {favorites.length > 0 ? (
                 favorites.map((item) => (
                     <div key={item.id}>
