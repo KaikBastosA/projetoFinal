93a4b983817d96e8a689d3dc8e4081d1519a43dc
ivanc <ivandelattre7@gmail.com>
2025-03-12 17:51:02 -0300

    feat : sale com retorno de quantidade

--- BackEnd/src/http/controllers/sale/get.ts
+++ BackEnd/src/http/controllers/sale/get.ts
@@ -3,6 +3,7 @@ import { z } from "zod"
 import { ResourceNotFoundError } from "../../../use-cases/errors/resource-not-found-error"
 import { PrismaSaleRepository } from "src/repositories/prisma/prisma-sale-repository"
 import { GetSaleUseCase } from "src/use-cases/sale/get-use-case"
+import { PrismaSale_PajamaRepository } from "src/repositories/prisma/prisma-salePajama.repository"
 
 export async function get(request: FastifyRequest,reply: FastifyReply) {
     const getParamsSchema = z.object({
@@ -12,14 +13,16 @@ export async function get(request: FastifyRequest,reply: FastifyReply) {
     const { id } = getParamsSchema.parse(request.params)
 
     try {
+        const sale_pajamaRepository = new PrismaSale_PajamaRepository()
         const prismaSaleRepository = new PrismaSaleRepository()
-        const getSaleUseCase = new GetSaleUseCase(prismaSaleRepository)
+        const getSaleUseCase = new GetSaleUseCase(prismaSaleRepository ,  sale_pajamaRepository  )
 
-        const {sale} = await getSaleUseCase.execute({
-            id    
-        })
-
-        return reply.status(200).send(sale)
+        const { sale, quantidade } = await getSaleUseCase.execute({
+            id
+        });
+    
+        return reply.status(200).send({ sale, quantidade });
+    
 
     } catch (err) {
         if (err instanceof ResourceNotFoundError){
--- BackEnd/src/use-cases/sale/get-use-case.ts
+++ BackEnd/src/use-cases/sale/get-use-case.ts
@@ -1,18 +1,20 @@
-import { Sale } from "@prisma/client"
+import { Sale, Sale_Pajamas } from "@prisma/client"
 import { ResourceNotFoundError } from "../errors/resource-not-found-error"
 import { PajamaSalesRepository } from "src/repositories/sales.repository"
+import { Sale_PajamaRepository } from "src/repositories/salesPajamas-repository"
 
 interface GetSaleUseCaseRequest {
     id: string
 }
 interface GetSaleUseCaseResponse {
     sale: Sale
+    quantidade : number
 }
 
 
 export class GetSaleUseCase{
 
-    constructor(private SaleRepository: PajamaSalesRepository) {}
+    constructor(private SaleRepository: PajamaSalesRepository , private sale_pajamaRepository : Sale_PajamaRepository ) {}
 
     async execute({id}: GetSaleUseCaseRequest): Promise<GetSaleUseCaseResponse>{
         const sale = await this.SaleRepository.get(id)
@@ -21,7 +23,14 @@ export class GetSaleUseCase{
             throw new ResourceNotFoundError()
         }
 
-        return { sale }
+        const table = await this.sale_pajamaRepository.listBySale(sale.id)
+        let quantidade = 0
+        for (const element of table) {
+            quantidade += element.quantity
+        }
+
+        return {  sale , quantidade }
+
     }
     
     
