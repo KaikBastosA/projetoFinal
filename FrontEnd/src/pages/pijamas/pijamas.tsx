309db51d2b67cfe475ed81c22908ebedbe5950de
LuizFelipeVVAlves <lfvilaverde@gmail.com>
2025-03-12 16:21:13 -0300

    feat: inicio desenv pagina de pijamas. Criando menu de filtros

--- FrontEnd/src/pages/pijamas/pijamas.tsx
+++ FrontEnd/src/pages/pijamas/pijamas.tsx
@@ -1,12 +1,51 @@
 import { useParams } from "react-router-dom"
+import s from './pijamas.module.css'
+import Select from 'react-select'
+import { useEffect } from "react";
 
 export default function Pijamas() {
+
+    var genero = [
+    { value: 'Todos', label: 'Todos' },
+    { value: 'Unissex', label: 'Unissex' },
+    { value: 'Masculino', label: 'Masculino' },
+    { value: 'Feminino', label: 'Feminino' },
+    { value: 'Família', label: 'Família' }
+    ];
+
+    var tipo = [
+        { value: 'Todos', label: 'Todos' },
+        { value: 'Adulto', label: 'Adulto' },
+        { value: 'Infantil', label: 'Infantil' }
+    ]
+
+    var estacao =[
+        {value: 'Todos', label: 'Todos'},
+        {value: 'Verao', label: 'Verao'},
+        {value: 'Inverno', label: 'Inverno'}
+    ]
+
     
-    var type = useParams().type
+    var typeParam = useParams().type
+
+    useEffect(() => {
+        
+    }, [])
     
     return (
         <div>
-            <h1>PIJAMAS {type}</h1>
+            <div className={s.filter_div}>
+                <div className={s.input_div}>
+                    <input type="text" name="" id="" placeholder="Pesquise pelo prduto"/>
+                    <button></button>
+                </div>
+            </div>
+            <div>
+                
+                <Select placeholder='Gênero' className={s.dropdown_menu} options={genero} isDisabled={typeParam === 'Masculino' || typeParam === 'Feminino'}></Select>
+                <Select placeholder="Tipo" className={s.dropdown_menu} options={tipo} isDisabled={typeParam === 'Infantil'}></Select>
+                <Select placeholder='Estação' className={s.dropdown_menu} options={estacao}></Select>
+            </div>
         </div>
     )
 }
