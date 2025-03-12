27fe9faba0e055daa7e02cc49331dad58f70b99f
Kaik <kaikbastos@id.uff.br>
2025-03-12 15:38:32 -0300

    (Fix & Style): Corrigido o slide tendo problemas para ser reduzido e centralizado o mesmo, faltando apenas a responsividade

--- FrontEnd/src/components/feedBackSlide/index.tsx
+++ FrontEnd/src/components/feedBackSlide/index.tsx
@@ -3,9 +3,9 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import '/node_modules/swiper/swiper-bundle.min.css';
-import { Navigation, Autoplay, Pagination } from 'swiper/modules';
-import FeedbackCard from '../feedbackCard';
+import { Navigation, Autoplay } from 'swiper/modules';
 import styles from './styles.module.css';
+import star from '../../assets/full-star.svg';
 
 interface Feedback {
     id: string;
@@ -20,8 +20,7 @@ export default function FeedbackSlider() {
     useEffect(() => {
         axios.get('http://localhost:3333/all-feedbacks')
             .then(response => {
-                const filteredFeedbacks = (response.data as Feedback[])
-                    .filter(feedback => feedback.rating >= 4);
+                const filteredFeedbacks = (response.data as Feedback[]).filter(feedback => feedback.rating >= 4);
                 setFeedbacks(filteredFeedbacks);
             })
             .catch(error => {
@@ -30,23 +29,50 @@ export default function FeedbackSlider() {
     }, []);
 
     return (
-<Swiper
-    modules={[Navigation, Autoplay, Pagination]}
-    spaceBetween={20}
-    slidesPerView={2}
-    loop={true}
-    pagination={{ clickable: true }}
-    navigation
-    autoplay={{ delay: 3000, disableOnInteraction: false }}
-    className={styles.feedbackContainer}
->
-    {feedbacks.map(feedback => (
-        <SwiperSlide key={feedback.id}>
-            <FeedbackCard feedback={feedback} />
-        </SwiperSlide>
-    ))}
-</Swiper>
+        <Swiper
+            modules={[Navigation, Autoplay]}
+            loop={true}
+            navigation
+            autoplay={{ delay: 3000, disableOnInteraction: false }}
+            className={styles.feedbackContainer}
+            breakpoints={{
+                1024: {
+                    slidesPerView: 3,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                480: {
+                    slidesPerView: 1,
+                },
+            }}
+        >
+            {feedbacks.map(feedback => (
+                <SwiperSlide key={feedback.id}>
+                    <FeedbackCard feedback={feedback} />
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
+}
 
+interface FeedbackCardProps {
+    feedback: Feedback;
+}
 
+function FeedbackCard({ feedback }: FeedbackCardProps) {
+    return (
+        <div className={styles.feedbackCardContainer}>
+        <div className={styles.feedbackCard}>
+            <h3>{feedback.name}</h3>
+            <div className={styles.stars}>
+                {Array.from({ length: Math.floor(feedback.rating) }).map((_, index) => (
+                    <img key={index} src={star} alt="star" />
+                ))}
+                {feedback.rating % 1 ? <img src={star} alt="half-star" /> : null}
+            </div>
+            <p>{feedback.description}</p>
+        </div>
+        </div>
     );
 }
