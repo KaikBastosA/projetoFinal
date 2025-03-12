e6b493ecf95dc0402a4431773798b6d09cb4b886
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-03-12 19:59:10 -0300

    (fix & style): página de carrinho componentizada e adicionada menu de navegação entre página carrinho e favoritos

--- FrontEnd/src/pages/carrinho/index.tsx
+++ FrontEnd/src/pages/carrinho/index.tsx
@@ -1,23 +1,46 @@
 import CarrinhoCard from "../../components/carrinhoCard";
+import carrinhoClicado from '../../assets/carrinho-de-comprar-icon.svg'
+import favoritoClicado from '../../assets/favorite-heart-icon.svg'
+import favoritoNaoClicado from '../../assets/favorite-icon.svg'
 import useCartStore from "../../stores/CartStore";
 import styles from './styles.module.css'
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Carrinho() {
 
-    const { cart } = useCartStore(); 
+    const { cart } = useCartStore()
+    const navigate = useNavigate() 
+    const [favoritoAtivo, setFavoritoAtivo] = useState(false)
 
     const calcularTotal = () => {
         return cart.reduce((total, produto) => total + produto.price * (produto.quantidade ?? 1), 0).toFixed(2) 
     }
 
+    function handleClick() {
+        setFavoritoAtivo(true)
+        navigate('/favoritos')
+    }
+
     return (
         <>
+
+        <div className={styles.nav}>
+            <button className={styles.carrinhoBotao}>
+                <img src={carrinhoClicado} alt="" />
+                Carrinho
+            </button>
+            <button className={styles.favoritoBotao} onClick={handleClick}>
+                <img src={favoritoAtivo ? favoritoClicado : favoritoNaoClicado} alt="" />
+                Favoritos
+            </button>
+        </div>
         
         <div className={styles.box}>
             <div className={styles.cardcart}>
                 {cart.length > 0 ? (
                     cart.map((produto) => (
-                        <CarrinhoCard pijama={{ ...produto, quantidade: produto.quantidade ?? 1, selectedSize: produto.selectedSize ?? 'indefinido' }} key={produto.id} /> 
+                        <CarrinhoCard pijama={{ ...produto, quantidade: produto.quantidade ?? 1, selectedSize: produto.selectedSize ?? 'indefinido', size: produto.size ?? [] }} key={produto.id} /> 
                     ))
                 ) : (
                     <p>O carrinho está vazio.</p>
@@ -26,8 +49,12 @@ export default function Carrinho() {
 
             {cart.length > 0 && (
                 <>
-                    <div className={styles.total}>
-                        <p>Total: R${calcularTotal()}</p>
+                    <div className={styles.comprar}>
+                        <div className={styles.total}>
+                            <h2>Total: </h2>
+                            <p>R${calcularTotal()}</p>
+                        </div>
+                            <button>COMPRE TUDO</button>
                     </div>
                 </>
             )}
