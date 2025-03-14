c1195190ad91a73b7b6af39a53e1f139aac1d6a3
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-03-13 23:07:46 -0300

    (fix & style): página de favoritos corrigida para exibir um slide de cards de favoritos

--- FrontEnd/src/pages/favoritos/index.tsx
+++ FrontEnd/src/pages/favoritos/index.tsx
@@ -5,8 +5,11 @@ import favoritoClicado from '../../assets/full-heart.svg'
 import styles from './styles.module.css'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import CardPajama from '../../components/cardPajama'
 import api from '../../api/api'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import '/node_modules/swiper/swiper-bundle.min.css';
+import { Navigation } from 'swiper/modules';
+import FavoritoCard from '../../components/favoritosCard'
 
 interface Pijama {
     id: string;
@@ -52,7 +55,7 @@ export default function Favoritos() {
     }
 
     return(
-        <div>
+        <div className={styles.pageContainer}>
             <div className={styles.nav}>
                 <button onClick={handleClick} className={styles.carrinhoBotao}>
                     <img src={carrinhoAtivo ? carrinhoClicado : carrinhoNaoClicado} alt="" />
@@ -63,19 +66,39 @@ export default function Favoritos() {
                     Favoritos
                 </button>
             </div>
-            <div className={styles.container}>
+
+            <div className={styles.swiperWrapper}>
                 {favorites.length > 0 ? (
-                    favorites.map((item) => {
-                        const pijamaCompleto = listaDePijamas.find(p => p.id === item.id); 
-
-                        if (!pijamaCompleto) return null; // Se não encontrar, não renderiza nada
-
-                        return (
-                            <div className={styles.box} key={item.id}>
-                                <CardPajama onToggleFavorite={() => handleToggleFavorite} pijama={pijamaCompleto} />
-                            </div>
-                        );
-                    })
+                    <>
+                    <Swiper
+                        modules={[Navigation]}
+                        loop={false}
+                        navigation
+                        className={styles.swiperContainer}
+                        spaceBetween={5}
+                        breakpoints={{
+                            1024: { slidesPerView: 6 },
+                            768: { slidesPerView: 4 },
+                            480: { slidesPerView: 3 },
+                        }}
+                    >
+                        {favorites.map((item) => {
+                            const pijamaCompleto = listaDePijamas.find(p => p.id === item.id);
+                            if (!pijamaCompleto) return null; // Se não encontrar, não renderiza nada
+                            
+                            return (
+                                <SwiperSlide key={item.id} className={styles.slide}>
+                                    <FavoritoCard
+                                        onToggleFavorite={() => handleToggleFavorite(item.id)}
+                                        pijama={pijamaCompleto}
+                                    />
+                                </SwiperSlide>
+                            );
+                        })
+                    }
+                    </Swiper>
+                  
+                    </>
                 ) : (
                     <p>Você ainda não tem favoritos.</p>
                 )}
